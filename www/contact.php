4007c28ca4e587b87dbd3c9cc8c35a4082eee69e
Aaron Siegel <datadreamerlabs@gmail.com>
2015-11-09 09:45:56 -0800

    unnecessary meta tag

--- www/contact.php
+++ www/contact.php
@@ -2,7 +2,6 @@
 <html class="no-js">
 	<head>
 		<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>datadreamer - contact aaron siegel</title>
 		<meta name="description" content="Computational information design, interactive art, and data visualization created over the past decade by Aaron Siegel.">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
