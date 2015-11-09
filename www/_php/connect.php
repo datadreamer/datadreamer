57b1bb4531998885b760007a242bf195ee92c153
Aaron Siegel <datadreamerlabs@gmail.com>
2015-11-09 10:26:53 -0800

    manage connections

---
+++ www/_php/connect.php
@@ -0,0 +1,12 @@
+// connect to db
+$db = mysql_connect("localhost", "asiegel_web", "buttslol!") or die("Failed to connect to server.");
+mysql_select_db("asiegel_blog") or die("Failed to select database.");
+
+// prep twitter stuff
+require_once 'vendor/twitter/twitter.class.php';
+
+// ENTER HERE YOUR CREDENTIALS (see credentials.txt)
+
+$twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+$statuses = $twitter->load(Twitter::ME);
+date_default_timezone_set('America/Los_Angeles');
