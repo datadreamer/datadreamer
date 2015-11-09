8a81ff4ba57f09b1bba10de4e5e247717e98875c
Aaron Siegel <datadreamerlabs@gmail.com>
2015-11-08 23:10:11 -0800

    Scaffolding about.php requirements.

---
+++ www/_php/header.php
@@ -0,0 +1,133 @@
+<!-- HEADER CONTAINS TITLE AND MENU -->
+
+<header>
+  <div id="logo" class="noselect">
+    <a href="/">datadreamer</a>
+  </div>
+  <button type="button" class="btn-navbar" onclick="toggleMenu()">
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+  </button>
+  <div id="sublogo" class="noselect">
+    The work of <a href="/about">Aaron Siegel</a>
+  </div>
+</header>
+
+<!-- MENU IS FIXED AND DISPLAY IS TOGGLED -->
+
+<div id="menu" class="noselect">
+  <hr>
+  <div id="menutitles">
+    <div class="menutitle" id="tweetmenutitle">
+      <div class="menutitletext">
+        Tweets
+      </div>
+    </div>
+    <div class="menutitle" id="postmenutitle">
+      <div class="vr vr2"></div>
+      <div class="menutitletext">
+        Posts
+      </div>
+    </div>
+    <div class="menutitle" id="tagmenutitle">
+      <div class="vr vr2"></div>
+      <div class="menutitletext">
+        Tags
+      </div>
+    </div>
+    <div class="menutitle" id="mainmenutitle">
+      <div class="vr vr2"></div>
+      <div class="menutitletext">
+        Menu
+      </div>
+    </div>
+  </div>
+
+  <!-- tweets -->
+
+  <div class="submenu" id="tweetmenu">
+    <?php
+      // list all tweets fetched when the page loaded.
+      foreach ($statuses as $status){
+        echo "<a href='http://twitter.com/datadreamer/status/{$status->id_str}'>";
+        echo "<div class='submenuitem'>";
+        echo "<div class='submenutext'>$status->text</div>";
+        echo "</div></a>";
+      }
+    ?>
+
+  </div>
+
+  <!-- posts -->
+
+  <div class="submenu" id="postmenu">
+    <div class="vr"></div>
+    <div class="submenuitemholder">
+      <?php
+        // list all blog posts as buttons to open permalinks.
+        $result = mysql_query("SELECT title,link,r,g,b FROM posts ORDER BY id DESC");
+        while($row = mysql_fetch_assoc($result)){
+          $title = $row['title'];
+          $link = $row['link'];
+          $r = $row['r'];
+          $g = $row['g'];
+          $b = $row['b'];
+          echo "<a href='http://www.datadreamer.com/blog/{$link}'>";
+          echo "<div class='submenuitem'>";
+          echo "<div class='submenutext' style='color:rgb({$r},{$g},{$b});'>{$title}</div>";
+          echo "</div></a>";
+        }
+      ?>
+    </div>
+  </div>
+
+  <!-- tags -->
+
+  <div class="submenu" id="tagmenu">
+    <div class="vr"></div>
+    <div class="submenuitemholder">
+      <?php
+        // list all tags as buttons to open list of relavent posts.
+        $result = mysql_query("SELECT tag, count(tag) AS num FROM tags GROUP BY tag ORDER BY tag ASC");
+        while($row = mysql_fetch_assoc($result)){
+          $tag = $row['tag'];
+          $num = $row['num'];
+          echo "<a href='/blog/tag/{$tag}'>";
+          echo "<div class='submenuitem tagmenuitem'>";
+          echo "<div class='submenutext tagmenutext'>{$tag} <font class='tagnum'>{$num}</font></div>";
+          echo "</div></a>";
+        }
+      ?>
+    </div>
+  </div>
+
+  <!-- main menu -->
+
+  <div class="submenu" id="mainmenu">
+    <div class="vr"></div>
+    <nav class="submenuitemholder">
+      <a href="/">
+        <div class="submenuitem mainmenuitem">
+          <div class="submenutext mainmenutext">Home</div>
+        </div>
+      </a>
+      <a href="http://datadreamer.com/blog">
+        <div class="submenuitem mainmenuitem">
+          <div class="submenutext mainmenutext">Blog</div>
+        </div>
+      </a>
+      <a href="http://datadreamer.com/about">
+        <div class="submenuitem mainmenuitem">
+          <div class="submenutext mainmenutext">About</div>
+        </div>
+      </a>
+      <a href="http://datadreamer.com/contact">
+        <div class="submenuitem mainmenuitem">
+          <div class="submenutext mainmenutext">Contact</div>
+        </div>
+      </a>
+    </nav>
+  </div>
+
+</div>
--- www/about.php
+++ www/about.php
@@ -1,67 +1,54 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html class="no-js">
 	<head>
+		<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>datadreamer - about aaron siegel</title>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<meta name="description" content="Computational information design, interactive art, and data visualization created over the past decade by Aaron Siegel.">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="_css/normalize.min.css" />
 		<link rel="stylesheet" type="text/css" href="_css/fonts.css" />
-		<link rel="stylesheet" type="text/css" href="_css/site.css" />
-		<script src="_js/jquery-1.9.1.min.js"></script>
-		<script src="_js/jquery.masonry.min.js"></script>
-		<script src="_js/site.js"></script>
-		
-		<script>
-			<?php
-				echo "type = 'canvas';";
-				echo "item = 'about';";
-			?>
-		</script>
-		
-		<?php
-			echo "<script src=\"_js/processing.min.js\"></script>";
-		?>
-		<script type="text/javascript">
-			// convenience function to get the id attribute of generated sketch html element
-			function getProcessingSketchId () { return 'aboutEmerge'; }
-		</script>
-		
+		<link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic' />
+		<link rel="stylesheet" type="text/css" href="_css/sitenew.css" />
+		<link rel="stylesheet" type="text/css" href="_css/about.css" />
+
+		<script src="_js/vendor/jquery-1.11.0.min.js"></script>
+    <script src="_js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<script src="blog/_js/menu.js"></script>
 	</head>
-	
+
 	<body>
-		
+
 		<?php
 			include_once("analyticstracking.php");
-			include("_html/header.html");
+			include("_php/header.php");
 		?>
-		
-		<div id="gallery">
-			<div id="galleryimage">
-				<div class='slideshow' id='slideshow'>
-					<img class="slide" src='_images/about_1_900.jpg' width='100%' height='100%' />
-				</div>
+
+		<div id="slideshow">
+			<div class="slide" id="slide1"></div>
+		</div>
+
+		<div id="content">
+			<div id="projecttitle" data-sr>
+				<h1>About Aaron Siegel</h1>
+			</div>
+			<div id="projectbody">
+				<p class="projectbodytext" data-sr>
+					Aaron Siegel is a transdisciplinarian with a concentration in computational information design. He received his BFA in Digital Media Art from the <a href="http://cadre.sjsu.edu">Cadre Laboratory for New Media</a> at San Jose State University in 2006, and his MFA in <a href="http://dma.ucla.edu">Design|Media Art</a> from the University of California, Los Angeles in 2008.
+					<br/><br/>
+					His motivation comes from interesting data sets, interfaces, and public spaces. He utilizes data visualization as a medium to explore complex systems, aiming to create aesthetic representations of data while fostering scientific empiricism. His work strives to display relationships and correlations within information systems that would remain unseen from any other perspective.
+					<br/><br/>
+					He has created work for various institutions including the <a href="http://jpl.nasa.gov">NASA Jet Propulsion Laboratory</a>, <a href="http://www.electroland.net">Electroland</a>, <a href="http://www.directedplay.com">Directed Play</a>, the <a href="http://senseable.mit.edu">MIT SENSEable City Laboratory</a>, <a href="http://www.facebook.com">Facebook</a> and <a href="http://www.fabrica.it">Fabrica</a>. He has exhibited work in Los Angeles, San Jose, San Francisco, Salt Lake City, Reno, Memphis, Indianapolis, Seattle, New York, Guadalajara, Madrid, Trieste, Heidelberg, Singapore and Rome. Aaron is currently adjunct faculty in the <a href="http://roski.usc.edu/">University of Southern California Roski School of Art and Design</a>.
+					<br/><br/>
+					<a href="/cv.pdf">Learn more</a> or <a href="/contact">contact</a>.
+				</p>
 			</div>
 		</div>
-		
-		<div id="contentcontainer">
-			<div id="contenttitle">
-				About Aaron Siegel
-			</div>
-			<div id="contenttext">
-				Aaron Siegel is a transdisciplinarian with a concentration in computational information design. He received his BFA in Digital Media Art from the <a href="http://cadre.sjsu.edu">Cadre Laboratory for New Media</a> at San Jose State University in 2006, and his MFA in <a href="http://dma.ucla.edu">Design|Media Art</a> from the University of California, Los Angeles in 2008.
-				<br/><br/>
-				His motivation comes from interesting data sets, interfaces, and public spaces. He utilizes data visualization as a medium to explore complex systems, aiming to create aesthetic representations of data while fostering scientific empiricism. His work strives to display relationships and correlations within information systems that would remain unseen from any other perspective.
-				<br/><br/>
-				He has created work for various institutions including the <a href="http://jpl.nasa.gov">NASA Jet Propulsion Laboratory</a>, <a href="http://www.electroland.net">Electroland</a>, <a href="http://www.directedplay.com">Directed Play</a>, the <a href="http://senseable.mit.edu">MIT SENSEable City Laboratory</a>, <a href="http://www.facebook.com">Facebook</a> and <a href="http://www.fabrica.it">Fabrica</a>. He has exhibited work in Los Angeles, San Jose, San Francisco, Salt Lake City, Reno, Memphis, Indianapolis, Seattle, New York, Guadalajara, Madrid, Trieste, Heidelberg, Singapore and Rome.
-				<br/><br/>
-				<a href="/cv.pdf">Learn more</a> or <a href="/contact">contact</a>.
-			</div>
-		</div>
-			
+
 		<?php
 			include("_html/footer.html");
 		?>
-    
+
 	</body>
 </html>
