520a253ba1006e1643acecf41743ef8c91167140
Aaron Siegel <datadreamerlabs@gmail.com>
2015-11-11 09:43:50 -0800

    Fixed connect.php and increased tweets

--- www/_php/connect.php
+++ www/_php/connect.php
@@ -1,3 +1,4 @@
+<?php
 // connect to db
 $db = mysql_connect("localhost", "asiegel_web", "buttslol!") or die("Failed to connect to server.");
 mysql_select_db("asiegel_blog") or die("Failed to select database.");
@@ -8,5 +9,6 @@ require_once 'vendor/twitter/twitter.class.php';
 // ENTER HERE YOUR CREDENTIALS (see credentials.txt)
 
 $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-$statuses = $twitter->load(Twitter::ME);
+$statuses = $twitter->load(Twitter::ME,30);
 date_default_timezone_set('America/Los_Angeles');
+?>
